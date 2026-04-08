68b60fc962333372d580618e5f25dc414fc0c55f
Rebecca Farlam <zdp520@york.ac.uk>
2026-04-08 15:13:21 +0100

    Word analysis

--- sentiment_analysis.R
+++ sentiment_analysis.R
@@ -8,6 +8,13 @@ library(argparse)
 library(ggpubr)
 })
 
+#Define global variables
+utils::globalVariables(c(
+  "language", "word", "content", "id", "created_at",
+  "sentiment", "score", "value", "method", "n"
+))
+
+
 load_data<-function(filename) {
   #Read the csv file into the variable "data"
   data <- read.csv(filename)
@@ -24,13 +31,29 @@ load_data<-function(filename) {
 }
 
 word_analysis<-function(toot_data, emotion) {
+  #Get the NRC sentiment lexicon
+  nrc <- get_sentiments("nrc")
+  #Only load the emotion sentiment
+  nrc <- nrc %>% filter(sentiment == emotion)
+  #Split all words into separate lines and link to id
+  words <- toot_data %>% unnest_tokens(word, content)
+  #Join to the NRC Lexicon using `inner_join()` so that only words that appear in both lists are kept
+  emotion_words <- words %>% inner_join(nrc, by = "word")
+  #Count how many times each word appears and get a top ten
+  top_words <- emotion_words %>%
+    group_by(word) %>% #Group rows by word
+    summarise(n = n()) %>% #Counts rows in each group
+    arrange(desc(n)) %>% #Sorts descending
+    slice_head(n = 10) #Selects top ten
 
-    return()
+  #Rejoin with the original data to bring back the `id`, `created_at`, and `sentiment` columns
+  result <- top_words %>% inner_join(emotion_words, by = "word")
+  return()
 }
 
 sentiment_analysis<-function(toot_data) {
 
-    return()
+  return()
 
 }
 
